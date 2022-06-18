4c559baba89490d5136824e4b553977f7e47e518
souhei1118 <so.takashi1118@gmail.com>
2022-06-18 13:52:21 +0000

    [paiza-ruby1-10]

--- chomp.rb
+++ chomp.rb
@@ -1,4 +1,5 @@
 # プログラムの実行時にデータを受け取る
+# 入力された値がnameに変数として入る
 
 name = gets.chomp
 puts "Hello #{name}"
---
+++ if.rb
@@ -0,0 +1,8 @@
+# 1. 標準入力から整数を 1 個受け取る
+# 2. その整数が 100 以下の場合、その数値を表示する
+
+number = gets.to_i
+
+if number <= 100
+  puts number
+end
