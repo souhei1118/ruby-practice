0eb1cd6fb74d903cdea920082e9c1b39056d6673
souhei1118 <so.takashi1118@gmail.com>
2022-06-21 05:15:10 +0000

    [paiza-ruby3-4]

---
+++ omikuji.rb
@@ -0,0 +1,13 @@
+omikuji = rand(1..10)
+
+if omikuji == 1
+    puts "大吉"
+elsif omikuji == 2
+    puts "中吉"
+elsif omikiji <= 4 #3,4の時
+    puts "小吉"
+elsif omikiji <=7 #5,6,7の時
+    puts "凶"
+else
+    puts "大凶" #8,9,10の時
+end
